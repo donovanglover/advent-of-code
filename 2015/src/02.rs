29cd9941365fe532253c71cb9fae0457f0914206
Donovan Glover <donovan@dglover.co>
2023-12-04 04:16:51 -0500

    2015: Add solution for day 2 part 2

--- 2015/src/02.rs
+++ 2015/src/02.rs
@@ -34,3 +34,38 @@ mod part1 {
         assert_eq!(super::i_was_told_there_would_be_no_math(&crate::input(file!())), 1588178);
     }
 }
+
+pub fn i_was_told_there_would_be_no_math_part2(str: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in str.lines() {
+        let mut lwh = vec![];
+
+        for num in line.split('x') {
+            if let Ok(num) = num.parse::<u32>() {
+                lwh.push(num);
+            }
+        }
+
+        lwh.sort();
+
+        sum += (2 * lwh[0]) + (2 * lwh[1]) + (lwh[0] * lwh[1] * lwh[2]);
+    }
+
+
+    sum
+}
+
+#[cfg(test)]
+mod part2 {
+    #[test]
+    fn example() {
+        assert_eq!(super::i_was_told_there_would_be_no_math_part2("2x3x4"), 34);
+        assert_eq!(super::i_was_told_there_would_be_no_math_part2("1x1x10"), 14);
+    }
+
+    #[test]
+    fn input() {
+        assert_eq!(super::i_was_told_there_would_be_no_math_part2(&crate::input(file!())), 3783758);
+    }
+}
