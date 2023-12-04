6975b1fe60e30b640e61fdd999a807f3ca4c88b1
Donovan Glover <donovan@dglover.co>
2023-12-04 16:29:12 -0500

    cleanup: Simplify and add panics

--- 2015/src/07.rs
+++ 2015/src/07.rs
@@ -4,58 +4,62 @@ pub fn some_assembly_required(str: &str) -> HashMap<&str, u16> {
     let mut map = HashMap::new();
 
     for line in str.lines() {
-        let mut words: Vec<&str> = line.split_whitespace().collect();
+        let words: Vec<&str> = line.split_whitespace().collect();
 
         match words.len() {
             3 => {
-                let last = *words.last().unwrap();
-                let first = *words.first().unwrap();
-
-                if let Ok(number) = first.parse::<u16>() {
-                    map.insert(last, number);
-                } else if let Some(b) = map.get(first) {
-                    map.insert(last, *b);
+                if let Ok(number) = words[0].parse::<u16>() {
+                    map.insert(words[2], number);
+                } else if let Some(number) = map.get(words[0]) {
+                    map.insert(words[2], *number);
+                } else {
+                    panic!("`{}` is undefined (assignment)", words[0])
                 }
             },
 
             4 => {
-                words.remove(0);
-
-                let last = *words.last().unwrap();
-                let first = *words.first().unwrap();
-
-                if let Ok(number) = first.parse::<u16>() {
-                    map.insert(last, !number);
-                } else if let Some(first) = map.get(first) {
-                    map.insert(last, !*first);
+                if let Ok(number) = words[1].parse::<u16>() {
+                    map.insert(words[3], !number);
+                } else if let Some(number) = map.get(words[1]) {
+                    map.insert(words[3], !*number);
+                } else {
+                    panic!("`{}` is undefined (not operator)", words[1])
                 }
             },
 
             5 => {
                 let num_left = match words[0].parse::<u16>() {
-                    Ok(number) => number,
-                    _ => *map.get(words[0]).unwrap()
+                    Ok(number) => Some(number),
+                    _ => map.get(words[0]).copied(),
                 };
 
                 let num_right = match words[2].parse::<u16>() {
-                    Ok(number) => number,
-                    _ => *map.get(words[2]).unwrap()
+                    Ok(number) => Some(number),
+                    _ => map.get(words[2]).copied(),
                 };
 
-                match words[1] {
-                    "AND" => {
-                        map.insert(words[4], num_left & num_right);
-                    },
-                    "OR" => {
-                        map.insert(words[4], num_left | num_right);
-                    },
-                    "LSHIFT" => {
-                        map.insert(words[4], num_left << num_right);
-                    },
-                    "RSHIFT" => {
-                        map.insert(words[4], num_left >> num_right);
-                    },
-                    _ => {}
+                if let Some(num_left) = num_left {
+                    if let Some(num_right) = num_right {
+                        match words[1] {
+                            "AND" => {
+                                map.insert(words[4], num_left & num_right);
+                            },
+                            "OR" => {
+                                map.insert(words[4], num_left | num_right);
+                            },
+                            "LSHIFT" => {
+                                map.insert(words[4], num_left << num_right);
+                            },
+                            "RSHIFT" => {
+                                map.insert(words[4], num_left >> num_right);
+                            },
+                            _ => {}
+                        }
+                    } else {
+                        panic!("words[2] `{}` has not been defined yet", words[2])
+                    }
+                } else {
+                    panic!("words[0] `{}` has not been defined yet", words[0])
                 }
             }
 
@@ -81,19 +85,19 @@ x LSHIFT 2 -> f
 y RSHIFT 2 -> g
 NOT x -> h
 NOT y -> i"), HashMap::from([
-("d", 72),
-("e", 507),
-("f", 492),
-("g", 114),
-("h", 65412),
-("i", 65079),
-("x", 123),
-("y", 456),
-]));
+            ("d", 72),
+            ("e", 507),
+            ("f", 492),
+            ("g", 114),
+            ("h", 65412),
+            ("i", 65079),
+            ("x", 123),
+            ("y", 456),
+        ]));
     }
 
     #[test]
     fn input() {
-        assert_eq!(*super::some_assembly_required(&crate::input(file!())).get("a").unwrap(), 9999u16);
+        assert_eq!(super::some_assembly_required(&crate::input(file!())), HashMap::new());
     }
 }
