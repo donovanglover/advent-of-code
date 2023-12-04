fe18572fdf367f9eee9022bcfbf0b65a355ebf19
Donovan Glover <donovan@dglover.co>
2023-12-04 00:50:16 -0500

    feat: Add solution for day 4 part 1

---
+++ src/04.rs
@@ -0,0 +1,58 @@
+pub fn scratchcards(str: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in str.lines() {
+        let mut is_winning = true;
+        let mut winning = vec![];
+        let mut points = 0;
+
+        for word in line.split(" ") {
+            if word == "Game" || word.contains(":") { continue; }
+
+            if word == "|" {
+                is_winning = false;
+                continue;
+            }
+
+            if let Ok(number) = word.parse::<u32>() {
+                if is_winning {
+                    winning.push(number);
+                    continue;
+                };
+
+                for card in &winning {
+                    if number == *card {
+                        if points > 0 {
+                            points *= 2;
+                        } else {
+                            points = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        sum += points;
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod part1 {
+    #[test]
+    fn example() {
+        assert_eq!(super::scratchcards("\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"), 13);
+    }
+
+    #[test]
+    fn input() {
+        assert_eq!(super::scratchcards(&crate::input(file!())), 20667);
+    }
+}
