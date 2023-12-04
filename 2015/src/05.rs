1179746c2751d0202e477ac72cab1ec7a02dd1f2
Donovan Glover <donovan@dglover.co>
2023-12-04 12:47:35 -0500

    2015: Add solution for day 5 part 2

--- 2015/src/05.rs
+++ 2015/src/05.rs
@@ -53,3 +53,58 @@ mod part1 {
         assert_eq!(super::doesnt_he_have_intern_elves_for_this(&crate::input(file!())), 258);
     }
 }
+
+pub fn doesnt_he_have_intern_elves_for_this_part2(str: &str) -> u32 {
+    let mut num_nice = 0;
+
+    for line in str.lines() {
+        let mut has_pair = false;
+        let mut has_repeat = false;
+
+        for (i, c1) in line.char_indices() {
+            has_repeat = has_repeat || c1 == line.chars().nth(i + 2).unwrap_or('.');
+
+            has_pair = has_pair || {
+                let mut pair = false;
+
+                if let Some(c2) = line.chars().nth(i + 1) {
+                    for j in (i + 2)..line.len() {
+                        if let Some(c4) = line.chars().nth(j + 1) {
+                            let c3 = line.chars().nth(j).unwrap_or('.');
+
+                            if c1 == c3 && c2 == c4 {
+                                pair = true;
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                pair
+            }
+        }
+
+        if has_pair && has_repeat {
+            num_nice += 1;
+        }
+    }
+
+    num_nice
+}
+
+#[cfg(test)]
+mod part2 {
+    #[test]
+    fn example() {
+        assert_eq!(super::doesnt_he_have_intern_elves_for_this_part2("qjhvhtzxzqqjkmpb"), 1);
+        assert_eq!(super::doesnt_he_have_intern_elves_for_this_part2("xxyxx"), 1);
+        assert_eq!(super::doesnt_he_have_intern_elves_for_this_part2("uurcxstgmygtbstg"), 0);
+        assert_eq!(super::doesnt_he_have_intern_elves_for_this_part2("ieodomkazucvgmuy"), 0);
+        assert_eq!(super::doesnt_he_have_intern_elves_for_this_part2("qjhvhtzxzqqjkmpb"), 1);
+    }
+
+    #[test]
+    fn input() {
+        assert_eq!(super::doesnt_he_have_intern_elves_for_this_part2(&crate::input(file!())), 53);
+    }
+}
