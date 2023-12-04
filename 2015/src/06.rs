2b7e9178acbed5b15e742660c453f37f6ec8a3f9
Donovan Glover <donovan@dglover.co>
2023-12-04 14:10:00 -0500

    2015: Add solution for day 6 part 2
    Note that the array had to be changed to a vector since otherwise a
    stack overflow would occur.
    
    https://users.rust-lang.org/t/what-can-i-do-to-avoid-thread-main-has-overflowed-its-stack-when-working-with-large-arrays/77091/4

--- 2015/src/06.rs
+++ 2015/src/06.rs
@@ -62,3 +62,79 @@ mod part1 {
         assert_eq!(super::probably_a_fire_hazard(&crate::input(file!())), 377891);
     }
 }
+
+pub fn probably_a_fire_hazard_part2(str: &str) -> usize {
+    let mut num_lights = 0;
+    let mut lights = vec![[0 as usize; 1000]; 1000];
+
+    for line in str.lines() {
+        let mut words = line.split(' ');
+        let mut toggle = false;
+        let mut turn_on = false;
+
+        match words.next() {
+            Some("toggle") => {
+                toggle = true;
+            },
+            Some("turn") => {
+                if let Some(maybe_on) = words.next() {
+                    turn_on = maybe_on == "on";
+                };
+            },
+            _ => {}
+        }
+
+        let a: Vec<&str> = match words.next() {
+            Some(c1) => c1.split(',').collect(),
+            None => vec![],
+        };
+
+        let b: Vec<&str> = match words.last() {
+            Some(c2) => c2.split(',').collect(),
+            None => vec![],
+        };
+
+        let x1: usize = a[0].parse().unwrap();
+        let y1: usize = a[1].parse().unwrap();
+        let x2: usize = b[0].parse().unwrap();
+        let y2: usize = b[1].parse().unwrap();
+
+        for i in x1..(x2 + 1) {
+            for j in y1..(y2 + 1) {
+                if toggle {
+                    lights[i][j] += 2;
+                    continue;
+                }
+
+                if turn_on {
+                    lights[i][j] += 1;
+                    continue;
+                }
+
+                if lights[i][j] > 0 {
+                    lights[i][j] -= 1;
+                }
+            }
+        }
+    }
+
+    for i in 0..lights.len() {
+        num_lights += lights[i].into_iter().sum::<usize>();
+    }
+
+    num_lights
+}
+
+#[cfg(test)]
+mod part2 {
+    #[test]
+    fn example() {
+        assert_eq!(super::probably_a_fire_hazard_part2("turn on 0,0 through 0,0"), 1);
+        assert_eq!(super::probably_a_fire_hazard_part2("toggle 0,0 through 999,999"), 2000000);
+    }
+
+    #[test]
+    fn input() {
+        assert_eq!(super::probably_a_fire_hazard_part2(&crate::input(file!())), 14110788);
+    }
+}
