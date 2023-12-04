cafb2733e42ea8d7d9cc14e76ce4b8e50182eef6
Donovan Glover <donovan@dglover.co>
2023-12-04 04:50:42 -0500

    2015: Add solution for day 3 part 2

--- 2015/src/03.rs
+++ 2015/src/03.rs
@@ -34,3 +34,44 @@ mod part1 {
         assert_eq!(super::perfectly_spherical_houses_in_a_vacuum(&crate::input(file!())), 2592);
     }
 }
+
+pub fn perfectly_spherical_houses_in_a_vacuum_part2(str: &str) -> usize {
+    let mut x = [0, 0];
+    let mut y = [0, 0];
+    let mut map = std::collections::HashMap::new();
+
+    map.insert(format!("0,0"), 0);
+
+    let mut n = 0;
+
+    for char in str.chars() {
+        match char {
+            '<' => x[n % 2] -= 1,
+            '>' => x[n % 2] += 1,
+            'v' => y[n % 2] -= 1,
+            '^' => y[n % 2] += 1,
+            _ => {}
+        };
+
+        map.insert(format!("{},{}", x[n % 2], y[n % 2]), 0);
+
+        n += 1;
+    }
+
+    map.len()
+}
+
+#[cfg(test)]
+mod part2 {
+    #[test]
+    fn example() {
+        assert_eq!(super::perfectly_spherical_houses_in_a_vacuum_part2("^v"), 3);
+        assert_eq!(super::perfectly_spherical_houses_in_a_vacuum_part2("^>v<"), 3);
+        assert_eq!(super::perfectly_spherical_houses_in_a_vacuum_part2("^v^v^v^v^v"), 11);
+    }
+
+    #[test]
+    fn input() {
+        assert_eq!(super::perfectly_spherical_houses_in_a_vacuum_part2(&crate::input(file!())), 2360);
+    }
+}
