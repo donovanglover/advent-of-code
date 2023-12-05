12a2602b3bc2d90a8d43245e3c26aff3d9247c8c
Donovan Glover <donovan@dglover.co>
2023-12-04 23:52:40 -0500

    chore: Remove unused function

--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -9,16 +9,3 @@ pub mod day3;
 
 #[path="04.rs"]
 pub mod day4;
-
-/// Given a `file!()`, return its input.
-pub fn input(file: &str) -> String {
-    if let Some(file) = std::path::Path::new(file).file_stem() {
-        if let Some(file) = file.to_str() {
-            if let Ok(input) = std::fs::read_to_string(format!("./input/{file}.txt")) {
-                return input;
-            }
-        }
-    }
-
-    String::new()
-}
